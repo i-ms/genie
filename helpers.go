c527476f5772b043526f6883ed43fce8099d561c
Mayank Singh <mayanksingh0068@gmail.com>
2021-11-25 16:02:38 +0530

    Random String Generation

--- helpers.go
+++ helpers.go
@@ -1,6 +1,24 @@
 package genie
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "qwertyuioplkjhgfdaszxcvbnmQWERTYUIOPLKJHBGVFCDXSAZNM1234567890+-"
+)
+
+// RandomString generates a random string length n from values in the const randomString
+func (g *Genie) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+	return string(s)
+}
 
 // CreateDirIfNotExists : creates directory
 // If there will be error , it will be off type error
