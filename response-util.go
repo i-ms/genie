b55e588a357ea9d22c41f989124d5249592bbc1e
Mayank Singh <mayanksingh0068@gmail.com>
2021-12-05 14:29:09 +0530

    response type : xml

--- response-util.go
+++ response-util.go
@@ -2,9 +2,11 @@ package genie
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"net/http"
 )
 
+// WriteJSON writes the data ot json
 func (g *Genie) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
 	out, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
@@ -18,6 +20,27 @@ func (g *Genie) WriteJSON(w http.ResponseWriter, status int, data interface{}, h
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (g *Genie) WriteXML(w http.ResponseWriter, status int, data interface{}, header ...http.Header) error {
+	out, err := xml.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(header) > 0 {
+		for key, value := range header[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
 	w.WriteHeader(status)
 	_, err = w.Write(out)
 	if err != nil {
